ef4b14440ab5c21bcaf37bf6428cc036c8f7c4d9
jnorgini <jnorgini@gmail.com>
2022-12-02 13:47:27 -0300

    genericos_delimitados

---
+++ src/generics_set_map/Aula2.java
@@ -0,0 +1,42 @@
+package generics_set_map;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import entities.Product;
+import services.CalculationService;
+
+public class Aula2 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		List<Product> list = new ArrayList<>();
+		
+		String patch = "C:\\Users\\julia\\OneDrive\\Área de Trabalho\\ADS\\Eclipse-Java\\ws-eclipse\\in.txt";
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(patch))) {
+			
+			String line = br.readLine();
+			while(line != null) {
+				String[] fields = line.split(",");
+				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+				line = br.readLine();
+			}
+			
+			Product x = CalculationService.max(list);
+			System.out.println("Most expensive:");
+			System.out.println(x);
+			
+		} catch(IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+
+	}
+
+}
